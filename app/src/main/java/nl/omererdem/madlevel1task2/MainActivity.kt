0b637536986f57a2c4327ba0425440e2aa53bd2d
Omer Erdem <m.o.erdem@outlook.com>
2020-09-05 09:22:28 +0200

    #3.2 Controller logic updated

--- app/src/main/java/nl/omererdem/madlevel1task2/MainActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel1task2/MainActivity.kt
@@ -2,6 +2,7 @@ package nl.omererdem.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import nl.omererdem.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -22,6 +23,27 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onSubmit() {
+        if (binding.input1.text.toString().toLowerCase() == "t") {
+            totalCorrectAnswers++
+        }
+        if (binding.input2.text.toString().toLowerCase() == "f") {
+            totalCorrectAnswers++
+        }
+        if (binding.input3.text.toString().toLowerCase() == "f") {
+            totalCorrectAnswers++
+        }
+        if (binding.input4.text.toString().toLowerCase() == "f") {
+            totalCorrectAnswers++
+        }
+        showToast()
+        resetScore()
+    }
 
+    private fun showToast() {
+        Toast.makeText(this, getString(R.string.correct_answer, totalCorrectAnswers), Toast.LENGTH_LONG).show()
+    }
+
+    private fun resetScore() {
+        totalCorrectAnswers = 0
     }
 }
