7bb651220589b65788f2fbac145be85b553c1866
Omer Erdem <m.o.erdem@outlook.com>
2020-09-05 09:37:30 +0200

    #3.3 Optimized controller code

--- app/src/main/java/nl/omererdem/madlevel1task2/MainActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel1task2/MainActivity.kt
@@ -2,12 +2,14 @@ package nl.omererdem.madlevel1task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import nl.omererdem.madlevel1task2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var totalCorrectAnswers: Int = 0
+    private var answersMap = emptyMap<EditText, String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,20 +22,19 @@ class MainActivity : AppCompatActivity() {
         binding.submitBtn.setOnClickListener {
             onSubmit()
         }
+        answersMap = mapOf(
+            binding.input1 to "t",
+            binding.input2 to "f",
+            binding.input3 to "f",
+            binding.input4 to "f"
+        )
     }
 
     private fun onSubmit() {
-        if (binding.input1.text.toString().toLowerCase() == "t") {
-            totalCorrectAnswers++
-        }
-        if (binding.input2.text.toString().toLowerCase() == "f") {
-            totalCorrectAnswers++
-        }
-        if (binding.input3.text.toString().toLowerCase() == "f") {
-            totalCorrectAnswers++
-        }
-        if (binding.input4.text.toString().toLowerCase() == "f") {
-            totalCorrectAnswers++
+        for ((input, answer) in answersMap) {
+            if (input.text.toString().toLowerCase() == answer) {
+                totalCorrectAnswers++
+            }
         }
         showToast()
         resetScore()
